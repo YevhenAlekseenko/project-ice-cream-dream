9d123eb7bb71d7f7cba494d2fd1013d04012c524
vkrupitsyn <vlad.krupitsyn@gmail.com>
2022-08-13 02:00:55 +0300

    add some vars and transition

--- src/js/masonry.js
+++
@@ -1,9 +0,0 @@
-var $grid = $('.grid').masonry({
-  columnWidth: 80,
-});
-// change size of item by toggling gigante class
-$grid.on('click', '.grid-item', function () {
-  $(this).toggleClass('gigante');
-  // trigger layout after item size changes
-  $grid.masonry('layout');
-});
